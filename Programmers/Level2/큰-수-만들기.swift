a1d6c5d5d1bda0cad7ee87b88e1ea97153294cb0
Jeon, Yeo Hun <hunihun956@gmail.com>
2021-07-25 02:46:42 +0900

    :sparkles: 2021-07-24 Solved

---
+++ Programmers/Level2/큰-수-만들기.swift
@@ -0,0 +1,86 @@
+
+// 재귀로 풀이
+import Foundation
+import Foundation
+
+func erase(_ number: inout [Character], _ k: inout Int, _ eraseCount: Int) -> [Character]{
+    let size = number.count
+    if k == eraseCount {
+        return number
+    }
+    
+    for i in 0..<number.count-1 {
+        if number[i] < number[i + 1] {
+            number.remove(at: i)
+            break
+        }
+    }
+    
+    if number.count == size {
+        number.removeLast()
+    }
+    
+    return erase(&number, &k, eraseCount + 1)
+}
+
+func solution(_ number:String, _ k:Int) -> String {
+    var nums = Array(number)
+    var eraseNum = k
+    
+    return String(erase(&nums, &eraseNum, 0))
+}
+
+
+// 반복문 풀이
+
+import Foundation
+func solution(_ number:String, _ k:Int) -> String {
+    var nums = Array(number)
+    var eraseNum = k
+    
+    while eraseNum != 0 {
+        let size = nums.count
+        for i in 0..<nums.count - 1 {
+            if nums[i] < nums[i + 1] {
+                nums.remove(at: i)
+                break
+            }
+        }
+        
+        if nums.count == size {
+            nums.removeLast()
+        }
+        
+        eraseNum -= 1
+    }
+    
+    
+    return String(nums)
+}
+
+
+//
+
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    let numbers = number.compactMap { Int(String($0)) }
+    var sub = [Int]()
+    var k = k
+
+    for (i, v) in numbers.enumerated() {
+        while k > 0, !sub.isEmpty, sub.last! < v {
+            k -= 1
+            sub.removeLast()
+        }
+
+        if k == 0 {
+            sub.append(contentsOf: numbers[i...])
+            break
+        } else {
+            sub.append(v)
+        }
+    }
+
+    return sub[..<(sub.count - k)].map { String($0) }.joined()
+}
