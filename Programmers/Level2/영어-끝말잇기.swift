e8a051633d1d9d6fd2cdb3917d7d7f74bb8298b7
Jeon, Yeo Hun <hunihun956@gmail.com>
2021-08-17 23:55:10 +0900

    2021-08-16 Solved

--- Programmers/Level2/영어-끝말잇기.swift
+++ Programmers/Level2/영어-끝말잇기.swift
@@ -4,7 +4,7 @@ func solution(_ n:Int, _ words:[String]) -> [Int] {
     var wordSet = Set<String>()
     for (index, word) in words.enumerated() {
         if !wordSet.isEmpty && (wordSet.contains(word) || words[index-1].last != word.first){
-            return [(index % n) + 1, index / n + 1]
+            return [index % n + 1, index / n + 1]
         }
         wordSet.insert(word)
     }
