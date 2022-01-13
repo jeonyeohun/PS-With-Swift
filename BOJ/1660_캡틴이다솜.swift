01dd7a9d4957033dc3585e419ec72cea18370ad6
jeonyeohun <21500630@handong.edu>
2022-01-13 22:14:35 +0900

    2021-12 Solved

---
+++ BOJ/1660_캡틴이다솜.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class FastIO {
+    private let buffer: [UInt8]
+    private var index: Int = 0
+    private let fhOutput: FileHandle = FileHandle.standardOutput
+    
+    init(fhInput: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fhInput.readToEnd()!) + [UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        if now == 45 {
+            isPositive.toggle(); now = read()
+        }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now - 48)
+            now = read()
+        }
+        return sum * (isPositive ? 1 : -1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 {
+            now = read()
+        }
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index - 1
+        while now != 10,
+              now != 32,
+              now != 0 {
+            now = read()
+        }
+        return Array(buffer[beginIndex..<(index - 1)])
+    }
+    
+    @inline(__always) func print(_ s: String) {
+        fhOutput.write(s.data(using: .utf8)!)
+    }
+}
+
+let fio = FastIO()
+let n = fio.readInt()
+
+var numOfShell = [0, 1]
+var dp = Array(repeating: 987654321, count: n + 1)
+var now = 2
+while numOfShell.last! <= n {
+    numOfShell.append(numOfShell[now - 1] * 2 - numOfShell[now - 2] + now)
+    now += 1
+}
+
+dp[0] = 0
+dp[1] = 1
+for i in 1...n {
+    for shell in numOfShell {
+        if shell <= i {
+            dp[i] = min(dp[i], dp[i-shell] + 1)
+        }
+    }
+}
+
+fio.print("\(dp[n])")
